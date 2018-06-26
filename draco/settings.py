8ece05aa90010bc50b92f72a05419d1ea8b6d0d9
Trebol <brageanth@gmail.com>
2018-06-26 15:32:20 -0500

    as

--- draco/settings.py
+++ draco/settings.py
@@ -163,7 +163,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'draco/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
